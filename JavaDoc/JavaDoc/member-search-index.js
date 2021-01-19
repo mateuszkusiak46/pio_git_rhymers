5ec28ab8dc690bcde1e8e30ff171f15a3759a42e
Mateusz Kusiak <222489@edu.p.lodz.pl>
2021-01-19 21:21:29 +0100

    Zadanie 1.5.6 - Stworzenie dokumentacji JavaDoc

---
+++ JavaDoc/JavaDoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"edu.kis.vh.nursery","c":"HanoiRhymer","l":"countIn(int)"},{"p":"edu.kis.vh.nursery","c":"HanoiRhymer","l":"HanoiRhymer()","u":"%3Cinit%3E()"},{"p":"edu.kis.vh.nursery","c":"HanoiRhymer","l":"reportRejected()"}];updateSearchResults();
--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,23 +1,32 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa obslugujaca stos
+ */
 public class HanoiRhymer extends DefaultCountingOutRhymer {
-
+	/**
+	 * Całkowita ilosc dodanych elementow
+	 */
 	private int totalRejected = 0;
 
+	/**
+	 *	Metoda zwracajaca liczbe dodanych elementow
+	 * @return Zwraca całkowita ilosc dodanych elementow
+	 */
 	public int reportRejected() {
 		return totalRejected;
 	}
 
+	/**
+	 *	Metoda zwiekszajaca stos
+	 * @param in Liczba dodawana do stosu
+	 */
 	@Override
 	public void countIn(int in) {
 		if (!callCheck() && in > peekaboo())
 			totalRejected++;
 		else
 			super.countIn(in);
-	}
-
-	public int getTotalRejected() {
-		return totalRejected;
 	}
 
 }
